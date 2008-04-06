a0b64981c8321e1c0afa38086696c3b1a13dfcf0
Alex Joni <alex_joni@users.sourceforge.net>
2008-04-06 16:55:00 +0000

    replace EMC_JOINT_LINEAR with EMC_LINEAR (same for ANGULAR), sometimes used for axes-related things too

--- src/emc/usr_intf/emcsh.cc
+++ src/emc/usr_intf/emcsh.cc
@@ -2280,10 +2280,10 @@ static int emc_joint_type(ClientData clientdata,
 	}
 
 	switch (emcStatus->motion.joint[joint].jointType) {
-	case EMC_JOINT_LINEAR:
+	case EMC_LINEAR:
 	    Tcl_SetResult(interp, "linear", TCL_VOLATILE);
 	    break;
-	case EMC_JOINT_ANGULAR:
+	case EMC_ANGULAR:
 	    Tcl_SetResult(interp, "angular", TCL_VOLATILE);
 	    break;
 	default:
@@ -2325,7 +2325,7 @@ static int emc_joint_units(ClientData clientdata,
 	}
 
 	switch (emcStatus->motion.joint[joint].jointType) {
-	case EMC_JOINT_LINEAR:
+	case EMC_LINEAR:
 	    /* try mm */
 	    if (CLOSE(emcStatus->motion.joint[joint].units, 1.0,
 		      LINEAR_CLOSENESS)) {
@@ -2350,7 +2350,7 @@ static int emc_joint_units(ClientData clientdata,
 	    return TCL_OK;
 	    break;
 
-	case EMC_JOINT_ANGULAR:
+	case EMC_ANGULAR:
 	    /* try degrees */
 	    if (CLOSE(emcStatus->motion.joint[joint].units, 1.0,
 		      ANGULAR_CLOSENESS)) {
