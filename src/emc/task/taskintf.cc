c5a2e44304d4fc35a6f16adc43c6451cdf53f42d
Dewey Garrett <dgarrett@panix.com>
2016-05-12 06:24:54 -0700

    taskintf.cc,homing.c bug fixes ini pins,special JA
    1) cut/paste error in taskintf.cc
    2) test for special_case_sync_all needs to be done
       after handling of sequences having mixed +/-
       values for same abs(sequence_number)
    
    thanks to pkmcnc for hexabot tests
    
    Signed-off-by: Dewey Garrett <dgarrett@panix.com>

--- src/emc/task/taskintf.cc
+++ src/emc/task/taskintf.cc
@@ -300,7 +300,7 @@ int emcJointUpdateHomingParams(int joint, double home, double offset)
 	return 0;
     }
 
-    emcmotCommand.command = EMCMOT_SET_JOINT_HOMING_PARAMS;
+    emcmotCommand.command = EMCMOT_UPDATE_JOINT_HOMING_PARAMS;
     emcmotCommand.joint = joint;
     emcmotCommand.home = home;
     emcmotCommand.offset = offset;
